26e51c9c39a2aa9db42bc633e042ffd5bce12272
Venkatesh <2021wb86507@wilp.bits-pilani.ac.in>
2025-07-07 18:38:58 +0530

    trigger CI

--- main/java/com/example/App.java
+++ main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello SonarQube");
+        System.out.println("Hello Devops Maven Project");
     }
 }
